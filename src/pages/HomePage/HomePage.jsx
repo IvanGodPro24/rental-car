e4dfe455000417ccc94976ba60da7f5ff6fe1e61
IvanGodPro24 <vanyanep@gmail.com>
2025-05-13 21:57:37 +0300

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import Container from "../../components/Container/Container";
+import css from "./HomePage.module.css";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <section className={css.section}>
+      <Container>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.paragraph}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <Link to='/catalog' className={css.view}>View Catalog</Link>
+      </Container>
+    </section>
+  );
+};
 
-export default HomePage
+export default HomePage;
